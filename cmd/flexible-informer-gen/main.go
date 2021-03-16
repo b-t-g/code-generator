d3a99c7bcd2cfdee663fa140b0d36283a8ce7427
Brendan Good <brendan@aspenmesh.io>
2021-03-16 17:31:03 -0600

    Modify informer generators to have List functions parse more liberally (#1)
    For CRDs that set preserveUnknownFields to true, informers can fail to sync because there is a mismatch between how strictly a resource is parsed between being put in kubernetes and how strictly a resource needs to be parsed before being put in a informer cache. This PR changes the List function to handle objects that don't parse properly differently by not including them in the returned list.

--- cmd/flexible-informer-gen/main.go
+++ cmd/flexible-informer-gen/main.go
@@ -19,9 +19,10 @@ package main
 import (
 	"flag"
 
+	"github.com/aspenmesh/code-generator/cmd/flexible-informer-gen/generators"
 	"github.com/spf13/pflag"
-	"k8s.io/code-generator/cmd/informer-gen/generators"
 	"k8s.io/code-generator/pkg/util"
+	"k8s.io/gengo/args"
 	"k8s.io/klog/v2"
 
 	generatorargs "k8s.io/code-generator/cmd/informer-gen/args"
